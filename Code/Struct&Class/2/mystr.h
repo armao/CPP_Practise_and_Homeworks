56743cfb5a58c5b15cca5d0c4b2a361926ce5c0f
Ahmao <suhsuan520@gmail.com>
2023-02-16 11:47:21 +0800

    Update mystr.h

--- Code/Struct&Class/2/mystr.h
+++ Code/Struct&Class/2/mystr.h
@@ -6,21 +6,30 @@ namespace my
 class str
 {
 public:
-    explicit account();
-    explicit account(float money);
-    explicit account(float money,const credit::creditLevel score);
-    float money() const;
-    float& money();
-    const char* showCreditScore() const;
-    void creditScore(credit::creditLevel score);
-    //存錢
-    void deposit(int deposit);
-    //領錢
-    bool withdraw(int withdraw);
-    //餘額
-    float balance() const;
+    str()
+    :
+    {
+    }
+    ~str()
+    :
+    {
+    }
+    
+    void append(char _char)
+    {
+        m_length += 1;
+    }
+    void append(char _char)
+    {
+        m_length += 1;
+    }
+    int len()
+    {
+        
+        return m_length;
+    }
 private:
-    float m_money;
-    const char* m_creditScore;
+    char m_sting[];
+    int m_length;
 };
 }
