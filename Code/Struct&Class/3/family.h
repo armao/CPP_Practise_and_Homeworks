5e9b218aab5be0bd2e909eac5b0a619cab3bf59b
Ahmao <suhsuan520@gmail.com>
2023-03-08 16:21:54 +0800

    Create family.h

---
+++ Code/Struct&Class/3/family.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "human.h"
+#include <vector>
+
+class family
+{
+public:
+	explicit family();
+	explicit family(human a_human);
+
+	void add_member(human a_human);
+
+	human& longest_name_member();
+
+	bool has_member_with_name(std::string a_string);
+
+	void print();
+
+private:
+	std::vector<human> m_member;
+
+};
+
