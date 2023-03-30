c51261fb948139b2171a65d0c5a21ee4d92bd843
Ahmao <suhsuan520@gmail.com>
2023-03-30 14:39:38 +0800

    Update kShape.h

--- Code/Homeworks/1/kShape.h
+++ Code/Homeworks/1/kShape.h
@@ -14,5 +14,11 @@ enum class kShape
 kShape stringToShape(const std::string& str);
 
 void cin_num_exception();
-void cin_num(const kShape& ks);
-void cin_shape(const std::string& str);
+
+void cin_num_rectangle();
+void cin_num_square();
+void cin_num_circle();
+void cin_num_circle();
+
+void cin_shape_switcherV1(const std::string& str);
+void cin_shape_switcherV2(const std::string& str);
